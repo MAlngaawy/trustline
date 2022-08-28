204fb3dacc9a61466ce9d179ddcf1d86e454ebfc
MAlngaawy <m.alngaawy@gmail.com>
2022-08-28 22:58:45 +0200

    add tailwind config colors

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App text-3xl font-bold underline">
+    <div className="App bg-trustGray h-screen">
+      <div className='text-3xl font-bold underline p-10 bg-trustBlue'>
+
       this is a react app with tailwind 
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        trustBlue1: '#51B4FF', //Sky blue
+        trustBlue2: '#019FFE', //Main blue
+        trustBlue3: '#152F4B', //Dark blue
+        trustBlue4: '#763BBF', //Purple
+        trustBlue5: '#351D4F', //Dark Purple
+        trustGray: '#F5F5F5' // The Gray Background
+      }
+    },
   },
   plugins: [],
 }
