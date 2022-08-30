39f339d057a097e47231e7a3d12cd2daf6e96f96
MAlngaawy <m.alngaawy@gmail.com>
2022-08-30 09:24:22 +0200

    add social media links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,19 @@
 import React from "react";
 import { ReactComponent as Logo } from "./assets/logo.svg";
+import { ReactComponent as Twitter } from "./assets/twitter.svg";
+import { ReactComponent as Linkedin } from "./assets/linkedin.svg";
 
 const Footer = () => {
   return (
     <div className="footer bg-black text-white py-10">
       <div className="container mx-auto border-t border-gray-400 flex flex-col-reverse md:flex-row items-center md:items-start justify-between pt-6">
         <div className="social flex gap-4 mt-6 md:mt-0">
-          <div className="twitter cursor-pointer">TT</div>
-          <div className="facebook cursor-pointer">FF</div>
+          <div className="twitter cursor-pointer">
+            <Twitter />
+          </div>
+          <div className="linkedin cursor-pointer">
+            <Linkedin />
+          </div>
         </div>
         <ul className="links font-bold flex flex-col gap-4">
           <li className="cursor-pointer">للشركات</li>
