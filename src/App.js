aaeead0eab7679518555ae016bf090e8afaa6517
MAlngaawy <m.alngaawy@gmail.com>
2022-08-30 15:34:14 +0200

    finish all posts page

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
 import { Posts } from "./pages/Posts";
+import { useEffect } from "react";
+import { useStateContext } from "./contexts/ContextProvider";
 
 function App() {
+  const { setData } = useStateContext();
+
+  useEffect(() => {
+    console.log("Effect");
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [setData]);
+
   return (
     <div className="App font-tajwal">
       <Routes>
--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,22 +1,14 @@
 import { Link } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
-import { useEffect } from "react";
 
 export const Posts = () => {
-  const { data, setData } = useStateContext();
-
-  useEffect(() => {
-    console.log("Effect");
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => setData(data));
-  }, [setData]);
+  const { data } = useStateContext();
 
   return (
     <div className="posts">
-      <div className="topbar py-10 bg-gray-400">
-        <Link to="/" className="font-bold relative">
-          <span className=" absolute -left-10 text-3xl">
+      <div className="topbar py-4 mb-10 bg-gray-400">
+        <Link to="/" className="font-bold relative ">
+          <span className=" absolute -left-10 ">
             <svg
               width="24"
               height="24"
@@ -30,12 +22,23 @@ export const Posts = () => {
         </Link>
       </div>
       <div className="content">
-        <div className="container">
+        <div className="container mx-auto flex justify-center items-center flex-col">
           {data.map((item) => {
-            return <h4> {item.title} </h4>;
+            return <SinglePost title={item.title} id={item.id} />;
           })}
         </div>
       </div>
+    </div>
+  );
+};
+
+const SinglePost = ({ title, id }) => {
+  return (
+    <div className="single my-4 border border-black hover:bg-gray-400 rounded-md w-full  ">
+      <Link className="text-left flex " to={`/${id}`}>
+        <div className="post-num p-4 bg-black text-white w-fit">{id}</div>
+        <h3 className="font-bold p-4">{title}</h3>
+      </Link>
     </div>
   );
 };
