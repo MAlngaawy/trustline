386e87b5c2920394fa6382f8f6bb04003f9e2189
MAlngaawy <m.alngaawy@gmail.com>
2022-08-30 23:24:19 +0200

    handle add post feature

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { useState } from "react";
+import { data } from "autoprefixer";
 
 export const Posts = () => {
   const { data, setData } = useStateContext();
-
+  const [newPost, setNewPost] = useState({
+    title: "",
+    body: "",
+    id: 0,
+  });
   const fetchData = () => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
@@ -29,15 +35,23 @@ export const Posts = () => {
       </div>
       <div className="content mx-4">
         <div className="container mx-auto flex justify-center items-center flex-col">
-          <button onClick={() => fetchData()}>
+          <button className=" border p-4" onClick={() => fetchData()}>
             Click To fetch data from the server
           </button>
+          <AddPost
+            setNewPost={setNewPost}
+            newPost={newPost}
+            setData={setData}
+            data={data}
+          />
           {data ? (
             data.map((item) => {
-              return <SinglePost title={item.title} id={item.id} />;
+              return (
+                <SinglePost key={item.id} title={item.title} id={item.id} />
+              );
             })
           ) : (
-            <div className="text-2xl my-4 font-medium border">
+            <div className="text-2xl my-4 font-medium">
               Click The button above to fetch data
             </div>
           )}
@@ -54,6 +68,65 @@ const SinglePost = ({ title, id }) => {
         <div className="post-num p-4 bg-black text-white w-fit">{id}</div>
         <h3 className="font-bold p-4">{title}</h3>
       </Link>
+    </div>
+  );
+};
+
+const AddPost = ({ newPost, setNewPost, setData, data }) => {
+  const updateTitle = (e) => {
+    setNewPost({
+      title: e.target.value,
+      body: newPost.body,
+    });
+  };
+
+  const updateBody = (e) => {
+    setNewPost({
+      title: newPost.title,
+      body: e.target.value,
+    });
+  };
+
+  console.log();
+  const submitPost = (e) => {
+    e.preventDefault();
+    setData([...data, { ...newPost, id: data[data.length - 1].id + 1 }]);
+    setNewPost({
+      title: "",
+      body: "",
+      id: 0,
+    });
+    console.log(data);
+  };
+
+  return (
+    <div className="add-post my-6">
+      <h2 className=" font-bold">Add new Post</h2>
+      <form
+        action="sunmit"
+        className="flex flex-col gap-3"
+        onSubmit={submitPost}
+      >
+        <input
+          className="border p-2"
+          type="text"
+          placeholder="Title"
+          value={newPost.title}
+          onChange={updateTitle}
+        />
+        <input
+          className="border p-2"
+          type="text"
+          value={newPost.body}
+          placeholder="Body"
+          onChange={updateBody}
+        />
+        <input
+          className="block bg-gray-600 text-white py-2 px-6"
+          type="submit"
+          value="Click"
+        />
+      </form>
     </div>
   );
 };
